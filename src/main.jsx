edec49c40e7dd763ffaaf426aedec23bef50b76a
malahimhaseeb <codewithhisham@gmail.com>
2025-12-11 21:56:24 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+const scene = new THREE.Scene();
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
+
+const camera = new THREE.PerspectiveCamera(36, window.innerWidth / window.innerHeight, 0.1, 30);
+camera.position.z = 5;
+
+const canvas = document.querySelector(".threejs");
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const renderLoop = () => {
+  controls.update()
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderLoop)
+}
+
+renderLoop()
+
